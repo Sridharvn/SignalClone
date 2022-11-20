31ff2e84b4bdf188026962011607085904a38884
Sridharvn <sridharvnampoothiripad@gmail.com>
2022-11-20 16:04:20 +0530

    Created message list component on TabOneScreen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,56 @@
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { Text, StyleSheet, View, Image } from "react-native";
+import { RootTabScreenProps } from "../types";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+// export default function TabOneScreen({
+//   navigation,
+// }: RootTabScreenProps<"TabOne">) {
+export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+        }}
+        style={styles.image}
+      />
+      <View style={styles.rightContainer}>
+        <View style={styles.row}>
+          <Text style={styles.name}>Elon Musk</Text>
+          <Text style={styles.text}>11:11 AM</Text>
+        </View>
+        <Text numberOfLines={1} style={styles.text}>
+          Message
+        </Text>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rightContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  image: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    marginRight: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 3,
+  },
+  text: {
+    color: "grey",
   },
 });
